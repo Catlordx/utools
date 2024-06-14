9ba28d34abdd17803d52f5c77f3c99d96260a4d5
Albert Caesor <nomorezzz@163.com>
2024-06-14 16:08:08 +0800

    feat: implement both sse and json

---
+++ src/errors.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum RequestError {
+    #[error("Failed to get API key")]
+    ApiKey,
+    #[error("HTTP request error: {0}")]
+    Request(#[from] reqwest::Error),
+    #[error("Failed to parse response as JSON: {0}")]
+    ParseJson(#[from] serde_json::Error),
+    #[error("Missing content in JSON response")]
+    MissingContent,
+}
+
